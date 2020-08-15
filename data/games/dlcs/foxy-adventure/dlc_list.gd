0a776ba6ad4a8b676f6ef7d04061cc17662f854f
MasterPolska123 <karoltomaszewskimusic@gmail.com>
2020-08-15 20:37:33 +0200

    another fix

--- data/games/dlcs/foxy-adventure/dlc_list.gd
+++ data/games/dlcs/foxy-adventure/dlc_list.gd
@@ -1,4 +1,4 @@
 extends Node
 var dlc_list = [
-	"https://dl.new-dev.tk/data/games/dlcs/foxy-adventure/dlc_cfg_files/dlc_test.cfg",#test
+	"https://dl.new-dev.tk/data/games/dlcs/foxy-adventure/dlc_cfg_files/dlc_test/dlc_test.cfg",#test
 ]
