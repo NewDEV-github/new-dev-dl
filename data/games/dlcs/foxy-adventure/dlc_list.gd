ca2d9f5320746838f02541604f78f8e4848e8f9a
MasterPolska123 <karoltomaszewskimusic@gmail.com>
2020-08-19 21:49:42 +0200

    added more test dlc files

--- data/games/dlcs/foxy-adventure/dlc_list.gd
+++ data/games/dlcs/foxy-adventure/dlc_list.gd
@@ -1,4 +1,6 @@
 extends Node
 var dlc_list = [
-	"https://dl.new-dev.tk/data/games/dlcs/foxy-adventure/dlc_cfg_files/dlc_test/dlc_test.cfg",#test
+	"https://dl.new-dev.tk/data/games/dlcs/foxy-adventure/dlc_cfg_files/dlc_test.cfg",#test
+	"https://dl.new-dev.tk/data/games/dlcs/foxy-adventure/dlc_cfg_files/dlc_classicsonic.cfg",#classic sonic
+	"https://dl.new-dev.tk/data/games/dlcs/foxy-adventure/dlc_cfg_files/dlc_new_exe.cfg",#new exe
 ]
